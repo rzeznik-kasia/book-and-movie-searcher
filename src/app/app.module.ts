140bdec3610b265e3042fdbe85b10fc803564c86
Katarzyna Rzeznik <rzeznik.kasia@gmail.com>
2020-07-12 01:08:21 +0200

    Bumps angular version to 8

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -18,7 +17,6 @@ import { CoreModule } from './core/core.module';
     AppRoutingModule,
     HttpClientModule,
     CoreModule,
-    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
