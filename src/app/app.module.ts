abfdf713b67363be30fb2779dcea5c3ea8ad26b9
Katarzyna Rzeznik <rzeznik.kasia@gmail.com>
2020-07-12 01:57:28 +0200

    Successfully adds ng-bootstrap library

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -17,6 +18,7 @@ import { CoreModule } from './core/core.module';
     AppRoutingModule,
     HttpClientModule,
     CoreModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
