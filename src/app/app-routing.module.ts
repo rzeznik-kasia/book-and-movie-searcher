00d347a6e02abf182241c3b937e3bbf88033434b
Katarzyna Rzeznik <rzeznik.kasia@gmail.com>
2020-07-13 22:43:47 +0200

    Adds favorites

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ const app_routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/books' },
   { path: 'books', loadChildren: () => import('./books/books.module').then(m => m.BooksModule) },
   { path: 'movies', loadChildren: () => import('./movies/movies.module').then(m => m.MoviesModule) },
+  { path: 'favorites', loadChildren: () => import('./favorites/favorites.module').then(m => m.FavoritesModule) },
   { path: '**', pathMatch: 'full', redirectTo: '/books' } 
 ];
 
---
+++ src/app/favorites/favorites.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+
+import { FavoritesRoutingModule } from './favorites-routing.module';
+
+@NgModule({
+  declarations: [FavoritesRoutingModule.components],
+  imports: [
+    FavoritesRoutingModule
+  ]
+})
+export class FavoritesModule { }
